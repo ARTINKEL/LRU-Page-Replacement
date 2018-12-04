df87a144100e2dae59f34ff3490d4bfe03cd104b
Austin TInkel <austin.tinkel@gmail.com>
2018-12-04 11:25:40 -0500

    initial commit

---
+++ src/LRU.java
@@ -0,0 +1,14 @@
+public class LRU extends ReplacementAlgorithm {
+
+    /**
+     * @param pageFrameCount - the number of physical page frames
+     */
+    public LRU(int pageFrameCount) {
+        super(pageFrameCount);
+    }
+
+    @Override
+    public void insert(int pageNumber) {
+
+    }
+}
